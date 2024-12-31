46dfa882cd511d4467dd55a325488a57826f4457
Nihil <35506793+Nihilnia@users.noreply.github.com>
2024-12-31 12:34:25 +0100

    v-once

---
+++ VUE/29_v-once/app.js
@@ -0,0 +1,37 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 44,
+      name: "",
+      confirmedName: "",
+    };
+  },
+  methods: {
+    handleAdd(numb) {
+      this.counter += numb;
+    },
+    handleRemove(numb) {
+      this.counter -= numb;
+    },
+    handleReset() {
+      this.counter = 0;
+    },
+    getName(event, arg) {
+      console.log(event);
+      this.name = `${arg} ${event.target.value}`;
+    },
+    handleSubmit(event) {
+      alert(
+        `${this.name}, ${this.counter}, ${event.target.firstElementChild.value}`
+      );
+    },
+    checkEnter() {
+      this.confirmedName = this.name;
+    },
+    checkEnterNoMore() {
+      this.confirmedName = "";
+    },
+  },
+});
+
+app.mount("#events");
