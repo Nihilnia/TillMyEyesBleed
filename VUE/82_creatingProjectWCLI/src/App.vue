26462167d03ace9730b919d9ade49f486c8a428d
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-01-07 19:20:02 +0100

    Run Nihil, do not stop son

---
+++ VUE/82_creatingProjectWCLI/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img src="./assets/Gloria.png" alt="" />
+  <RunNihil />
+</template>
+
+<script>
+import RunNihil from "./components/RunNihil.vue";
+
+export default {
+  name: "App",
+  components: {
+    RunNihil,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  max-width: 50%;
+  border-radius: 10px;
+}
+</style>
---
+++ VUE/82_creatingProjectWCLI/src/components/RunNihil.vue
@@ -0,0 +1,10 @@
+<template>
+  <h3>Run Nihil</h3>
+  <h2>Do not stop son</h2>
+</template>
+
+<script>
+export default {
+  name: "RunNihil",
+};
+</script>
