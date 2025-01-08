6c3b8560052bfa5335e83caee5cce76709cb0f84
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-01-08 14:29:02 +0100

    Like a baby' s first steps

---
+++ VUE/87_aNewProject/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li v-for="friend in friends" :key="friend.id">
+      <h3>{{ friend.id }}</h3>
+      <h3>{{ friend.name }}</h3>
+      <h3>{{ friend.phone }}</h3>
+      <h3>{{ friend.eMail }}</h3>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 0,
+          name: "Gloria",
+          phone: "123 456",
+          eMail: "Gloria@AI.com",
+        },
+        {
+          id: 1,
+          name: "ALT",
+          phone: "654 321",
+          eMail: "ALT@rogueAI.com",
+        },
+      ],
+    };
+  },
+};
+</script>
