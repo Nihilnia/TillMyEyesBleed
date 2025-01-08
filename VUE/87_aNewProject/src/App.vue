0a2cc91fe16d2217e8eaa824a7dbb45de1943fc6
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-01-08 15:12:40 +0100

    Smack My Bitch Up

--- VUE/87_aNewProject/src/App.vue
+++ VUE/87_aNewProject/src/App.vue
@@ -1,33 +1,80 @@
 <template>
-  <ul>
-    <li v-for="friend in friends" :key="friend.id">
-      <h3>{{ friend.id }}</h3>
-      <h3>{{ friend.name }}</h3>
-      <h3>{{ friend.phone }}</h3>
-      <h3>{{ friend.eMail }}</h3>
-    </li>
-  </ul>
+  <section>
+    <header>
+      <h1>Friends List</h1>
+    </header>
+    <friend-contact name="Gloria"></friend-contact>
+    <friend-contact name="Alt"></friend-contact>
+  </section>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      friends: [
-        {
-          id: 0,
-          name: "Gloria",
-          phone: "123 456",
-          eMail: "Gloria@AI.com",
-        },
-        {
-          id: 1,
-          name: "ALT",
-          phone: "654 321",
-          eMail: "ALT@rogueAI.com",
-        },
-      ],
-    };
-  },
-};
-</script>
+<script></script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+  list-style: none;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ VUE/87_aNewProject/src/components/FriendContact.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleVisibility">
+        {{ isDetailsVisible ? "hide details" : "show details" }}
+      </button>
+      <ul v-if="isDetailsVisible">
+        <li>{{ friend.phone }}</li>
+        <li>{{ friend.eMail }}</li>
+      </ul>
+    </li>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+  },
+  data() {
+    return {
+      isDetailsVisible: false,
+      friend: {
+        id: 0,
+        name: this.name,
+        phone: "123 456",
+        eMail: `${this.name}@AI.com`,
+      },
+    };
+  },
+  methods: {
+    toggleVisibility() {
+      this.isDetailsVisible = !this.isDetailsVisible;
+    },
+  },
+  watch: {
+    name(val) {
+      return val == "Gloria"
+        ? (this.friend.eMail = "Gloria@AI.com")
+        : (this.friend.eMail = "Alt@RogueAI.com");
+    },
+  },
+};
+</script>
+
+<style></style>
